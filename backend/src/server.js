703534471165ec1e055048748766c59d8ba0e29e
Kishor Joshi <kishorjoshiabd@gmail.com>
2025-08-23 16:06:21 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { ENV } from "./config/env.js";
+const app = express();
+
+const port = ENV.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} at http://localhost:${port}`);
+});
