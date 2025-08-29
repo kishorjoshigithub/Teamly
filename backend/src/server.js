5599a9d39b86158617c2a1a8130ca2985408d16c
Kishor Joshi <kishorjoshiabd@gmail.com>
2025-08-29 09:13:39 +0530

    db connected and env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { ENV } from "./config/env.js";
+import connectDB from "./config/db.js";
 const app = express();
 
 const port = ENV.PORT;
@@ -10,14 +11,12 @@ const port = ENV.PORT;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-
 //Routes
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} at http://localhost:${port}`);
+  connectDB();
 });
